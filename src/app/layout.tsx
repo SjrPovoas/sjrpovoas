d5f75130a50440032428dc7c8cc77a56fafae218
SjrPovoas <87617524+SjrPovoas@users.noreply.github.com>
2025-12-09 03:41:20 -0300

    update-correcao-deploy2.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"], });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"], });
 
-export const metadata: Metadata = { title: "SjrPovoaS", description: "Criamos a solução que você procura", };
+export const metadata: Metadata = { title: "SjrPovoaS", description: "Criamos a solução que você procura. Planos de assinatura e conteúdo exclusivo.", icons: {
+  icon: 'src/app/favicon.ico' } };
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="pt-BR" prefix="og: https://ogp.me/ns#">
+    <html lang="pt-BR" prefix="og: https://ogp.me/ns#" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
